4063c244f7574dcb24600ea1bf34e2e891b9948c
inisik <inisik0820@gmail.com>
2018-04-28 13:10:10 -0400

    Coveralls configuration is not needed if not enabled. (#14)
    * Coveralls configuration is not needed if not enabled.
    
    * Remove logging if coveralls is not enabled.
    
    * Coveralls is not enabled.
    
    * Clean file and fix docblock contradictions.
    
    * Delete extra line.
    
    * Fix missed docblock.

--- app_config.php
+++ app_config.php
@@ -2,80 +2,74 @@
 use limeberry\Configuration;
 
 /**
-*	=== [Environment Configuration ] ===
-*	This configuration is used to set the 
-*	development environment for your project.
-*	You can set 2 types of environments;
-*	"development" OR "publish"
-**/
+ * === [Environment Configuration ] ===
+ * This configuration is used to set the 
+ * development environment for your project.
+ * You can set 2 types of environments;
+ * "development" OR "publish"
+ */
 Configuration::setEnvironment("development");
 
-
-
 /**
-*	=== [Application Folder ] ===
-*	This configuration is generally for security.
-*	If you need to change the folder name of your
-*	application you must set it here.
-**/
+ * === [Application Folder ] ===
+ * This configuration is generally for security.
+ * If you need to change the folder name of your
+ * application you must set it here.
+ */
 Configuration::setApplicationFolder("application");
 
-
 /**
-*	=== [Application URL ] ===
-*	This configuration is used to set your
-*	web sites url. For example if you create a
-*	application that will be run in root set url to: "http://www.examplesite.com/"
-*	If your application will be a sub module/application of other application set it like:
-*	 "http://www.examplesite.com/my_module". 
-**/
+ * === [Application URL ] ===
+ * This configuration is used to set your
+ * web sites url. For example if you create a
+ * application that will be run in root set url to: "http://www.examplesite.com/"
+ * If your application will be a sub module/application of other application set it like:
+ * "http://www.examplesite.com/my_module". 
+ */
 Configuration::setApplicationUrl("http://localhost/limeberry");
 
-
-
 /**
-*	=== [Is Application Root ] ===
-*	If your application's url is not accessable by "www.siteexample.com"
-*	set this false. 
-**/
+ * === [Is Application Root ] ===
+ * If your application's url is not accessable by "www.siteexample.com"
+ * set this false. 
+ */
 Configuration::setRoot(false);
 
-
 /**
- *      === [ Application Name ] ===
- *      This configuration is used to set name of your application.
+ * === [ Application Name ] ===
+ * This configuration is used to set name of your application.
  */
 Configuration::setTitle("EmptyApp");
 
 /**
- *      === [ Application Version ] ===
- *      This configuration is used to set version number of your application.
+ * === [ Application Version ] ===
+ * This configuration is used to set version number of your application.
  */
 Configuration::setVersion("1.0.0");
 
 /**
- *      === [ Application Description ] ===
- *      This configuration is used to set description of your application.
+ * === [ Application Description ] ===
+ * This configuration is used to set description of your application.
  */
 Configuration::setDescription("AppDescription");
 
-//Configuration::setUrlProtected(TRUE);
-//Configuration::UnwantedParameters(array("'", "-"));
+// Configuration::setUrlProtected(TRUE);
+// Configuration::UnwantedParameters(array("'", "-"));
 
-
-/****************************
-*	Application Class
-*****************************/
+/**-------------------*/
+/** Application Class */
+/**-------------------*/
 class Application
 {
-    
+
     /**
      * This method is called when your application is loaded. You can register
      * your object mappers or do other configurations.
+     *
+     * @return mixed|void Return anything or nothing.
      */
     public function Register()
     {
-        
     }
 }
 ?>
